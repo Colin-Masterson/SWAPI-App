4058f4f5cf8a2458d1185147fb09b69b46802b9e
Colin Masterson <colinmasterson97@hotmail.com>
2022-02-16 19:50:29 +0000

    Pulled in data from SWAPI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { useState, useEffect } from 'react';
+import Table from './Components/DataTable';
+
 function App() {
-    return <div className='App'></div>;
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const url = 'https://swapi.dev/api/people/';
+
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                const json = await response.json();
+                console.log(json.results);
+                setData(json.results);
+            } catch (error) {
+                console.log('error', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className='App'>
+            <Table data={data} />
+        </div>
+    );
 }
 
 export default App;
