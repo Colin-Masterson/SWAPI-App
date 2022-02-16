95a485514e996c86bc6cefd6f9360abebe77d962
Colin Masterson <colinmasterson97@hotmail.com>
2022-02-16 22:15:43 +0000

    Pulling in all people

--- src/App.js
+++ src/App.js
@@ -15,10 +15,49 @@ function App() {
 
         const fetchData = async () => {
             try {
-                const response = await fetch(url);
-                const json = await response.json();
+                const res1 = await fetch(url);
+                const json1 = await res1.json();
 
-                setData(json.results);
+                const res2 = await fetch(json1.next);
+                const json2 = await res2.json();
+
+                const res3 = await fetch(json2.next);
+                const json3 = await res3.json();
+
+                const res4 = await fetch(json3.next);
+                const json4 = await res4.json();
+
+                const res5 = await fetch(json4.next);
+                const json5 = await res5.json();
+
+                const res6 = await fetch(json5.next);
+                const json6 = await res6.json();
+
+                const res7 = await fetch(json6.next);
+                const json7 = await res7.json();
+
+                const res8 = await fetch(json7.next);
+                const json8 = await res8.json();
+
+                const res9 = await fetch(json8.next);
+                const json9 = await res9.json();
+
+                const all = [
+                    ...json1.results,
+                    ...json2.results,
+                    ...json3.results,
+                    ...json4.results,
+                    ...json5.results,
+                    ...json6.results,
+                    ...json6.results,
+                    ...json7.results,
+                    ...json8.results,
+                    ...json9.results,
+                ];
+
+                setData(all);
+
+                console.log(all);
             } catch (error) {
                 console.log('error', error);
             }
